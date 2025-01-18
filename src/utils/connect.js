3825fc792ce311b1109682bfce030788125669a0
joshua Takyi <takyijoshua019@gmail.com>
2025-01-18 22:07:53 +0000

    type module

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -12,16 +12,9 @@ const ConnectDb = async () => {
 	const fullUri = process.env.MONGODB_URI;
 
 	try {
-		const connection = await mongoose.connect(fullUri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			maxPoolSize: 10, // Use a connection pool of 10 connections
-			serverSelectionTimeoutMS: 5000, // Faster failover if the server is unavailable
-			socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-		});
-
+		const connection = await mongoose.connect(fullUri);
 		logger.info("Connected to MongoDB");
-		cachedConnection = connection; // Cache the connection
+		cachedConnection = connection;
 		return connection;
 	} catch (error) {
 		logger.error("Failed to connect to MongoDB:", error);
